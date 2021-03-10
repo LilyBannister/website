a0ee48c0ecff445bde06397f372111d429606627
Lily Bannister <79328208+LilyBannister@users.noreply.github.com>
2021-03-10 18:35:35 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+  
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Lily!'
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
