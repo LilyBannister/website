6f393178556deda22c5deb27fe777ae86f5f9de5
Jan Bannister <jan.bannister@gmail.com>
2021-03-27 18:45:36 +0000

    web - init

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-  
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Lily!'
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
